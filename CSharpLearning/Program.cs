6856f44ad233fb862eff931812d0562cc700f5ec
Lars Ruth <lars_ruth@outlook.de>
2018-11-30 14:07:09 +0100

    Update Program.cs
    With Threadpool

--- CSharpLearning/Program.cs
+++ CSharpLearning/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace CSharpLearning
 {
@@ -11,6 +11,8 @@ namespace CSharpLearning
 	{
 		private static string baseUrl = "";
 		private static HashSet<string> checkedLinks = new HashSet<string>();
+		private static int workersActive = 0;
+		private static AutoResetEvent resetEvent = new AutoResetEvent(false);
 
 		public static void CountWordsOnPageRecursive(string url, int depth = 3)
 		{
@@ -29,16 +31,29 @@ namespace CSharpLearning
 				}
 			}
 
-			Console.WriteLine("{0}: {1}", url, Regex.Matches(content, "Game", RegexOptions.IgnoreCase).Count);
-			checkedLinks.Add(url);
+			Console.WriteLine("{2} -> {0}: {1}", url, Regex.Matches(content, "Game", RegexOptions.IgnoreCase).Count, depth);
 
 			if (depth >= 0)
 			{
+				//List<Thread> startedThreads = new List<Thread>();
+
 				foreach (Match match in Regex.Matches(content, "href=\"/([^\"#\\?:.]*)[\"#\\?]"))
 				{
 					var link = baseUrl + match.Groups[1].Value;
 
-					if (!checkedLinks.Contains(link)) CountWordsOnPageRecursive(link, depth - 1);
+					lock (checkedLinks)
+					{
+						if (checkedLinks.Contains(link)) continue;
+						checkedLinks.Add(link);
+					}
+
+					Interlocked.Increment(ref workersActive);
+					ThreadPool.QueueUserWorkItem(state => 
+					{
+						CountWordsOnPageRecursive(link, depth - 1);
+						Interlocked.Decrement(ref workersActive);
+						if (workersActive == 0) resetEvent.Set();
+					});
 				}
 			}
 		}
@@ -47,12 +62,15 @@ namespace CSharpLearning
 		{
 			DateTime begin = DateTime.UtcNow;
 			baseUrl = "http://www.games-academy.de/";
+			checkedLinks.Add(baseUrl);
+
 			CountWordsOnPageRecursive(baseUrl);
 
-			DateTime end = DateTime.UtcNow;
+			if(workersActive != 0) resetEvent.WaitOne();
 
+			DateTime end = DateTime.UtcNow;
 			Console.WriteLine("Finished: {0}ms", (end - begin).TotalMilliseconds);
-			
+
 			Console.ReadKey();
 		}
 	}
